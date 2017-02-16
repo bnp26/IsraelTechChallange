03ab27b6722058cd58fbf091bfcfd341e8403af8
bnp26 <bnp26@case.edu>
2017-02-16 14:16:15 -0500

    almost done

--- challange_2/fellows_ch1_songs_benjamin_poreh.py
+++ challange_2/fellows_ch1_songs_benjamin_poreh.py
@@ -1,39 +1,44 @@
-from random import Random
+import random
 
+#class Song so as to remember the songs name
+#Song only has a string in it.
 class Song:
     def __init__(self):
         self.name = ""
-        
+
+    def __init__(self, put_name):
+	self.name = put_name
+
+    def __repr__(self):
+	return self.name
+#class Playlist
+#allows you to put songs into the playlist via constructor
+#run gen_rand_playlist to generate a randomized playlist
 class Playlist:
     def __init__(self, passed_songs):
         if passed_songs == None:
             self.songs = dict()
         else:
             self.songs = passed_songs
-            
+    
+    def __repr__(self):
+	return str(self.songs.values())
+
     def gen_rand_playlist(self):
         randomized = dict()
-        for song in self.songs:
-            ran_num = Random.random()
-            randomized[ran_num] = song
-            key_list = randomized.keys()
-            length = len(randomized)
-            
-def find_loc (key_num_list, rand, pivot):
-    if rand > key_num_list[pivot]:
-        return find_loc(key_num_list, rand, pivot*1.5)
-    elif rand < key_num_list[pivot]:
-        return find_loc(key_num_list, rand, pivot/1.5)
-    else:
-        return pivot
+        size = len(self.songs)
+	for song in self.songs:
+            ran_num = random.random()
+	    new_index = int(ran_num * 100)
+	    randomized.setdefault(new_index, []).append(song)
+	return randomized.values()
 
-
+#to test, add songs in the style of {1:Song("Song Name"), 2:Song("Next Song"), 3:Song("Etc.")}
 def main():
     songs = {1:Song("BabyGotBack"), 2:Song("TwoStep"), 3:Song("ShitFuckStack"), 4:Song("EnormousPenis")}
     playlist = Playlist(songs)
-    print str(playlist.gen_rand_playlist())
-    
-    
-    
-    
-    
+    final = playlist.gen_rand_playlist()
+    print final
+
+if __name__ == "__main__":
+    main()
