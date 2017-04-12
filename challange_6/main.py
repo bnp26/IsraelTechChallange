26694a491c9aa0f144b1c88d37ee22571c72aef7
bnp26 <bnp26@case.edu>
2017-04-11 20:38:36 -0400

    almost done with merge challange

--- challange_6/main.py
+++ challange_6/main.py
@@ -1,34 +1,65 @@
 import os
+import sys
 
 def merge_sort(nums1, nums2):
     merged = []
     x = 0
-    c1 = 0
-    c2 = 0
+    a = 0
+    b = 0
     while x < len(nums1) + len(nums2):
-	if nums1
+	if nums1[a] <= nums2[b]:
+	    merged.append(nums1[a])
+	    a += 1
+	else:
+	    merged.append(nums2[b])
+	    b += 1
+	x+=1
+    return merged
+
+def get_nums(line):
+    nums = []
+    line_ending = ""
+    if line[:len(line)-2] != '\r\n':
+	line_ending = '\r\n'
+    else:
+	line_ending = '\n'
+    while len(line) >=2:
+	str_split = line.split("\t",1)
+	nums.append(int(str_split[0]))
+	if len(str_split) == 1:
+	    break
+	else:
+	    line = str_split[1]
+    return nums
+
+
 
 def main(input_file, output_file):
     nums_file = open(input_file, 'r')
     out = open(output_file, 'w')
     
-    nums = []
+    nums1 = []
+    nums2 = []
     lines = nums_file.readlines()
-    for line in lines:
-	line_ending = ""
-	if line[:len(line)-2] != '\r\n':
-	    line_ending = '\r\n'
+    x = 0
+    while lines[x] != None:
+	if x == 0:
+	    nums1.append(get_nums(lines[x]))
 	else:
-	    line_ending = '\n'
-        while line != line_ending:
-            str_split = line.split("\t",1)
-	    nums.append(int(str_split[0]))
-	    if len(str_split) == 1:
-		break
-	    else:
-		line = str_split[1]
-    nums_file.close() 
+	    nums2.append(get_nums(lines[x]))
+    merged = merge_sort(nums1, nums2)
+    nums_file.close()
+    for x in range(0, len(merged)):
+	if x < len(merged):
+	    out.write(merged[x])
+	    out.write('\t')
+	else:
+	    out.write(merged[x])
+	    out.write('\r\n')
+    out.close()
 
+	
+	
 if __name__ == "__main__":
     input_file = ''
     output_file = ''
